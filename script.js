a061c81b9b4e37b587f241f102320ae1075020b7
bluartie <arteaga.jorge3@gmail.com>
2022-11-03 21:28:13 -0500

    get all four operations to work. There's a bug where subtraction always outputs a subtraction symbol followed by the answer

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let displayScreen = document.querySelector(".current-operation");
+
 /*
 create operator functions
 addeventlisteners to buttons, output button events to screen
@@ -15,30 +17,49 @@ for solve():
 parse operator from display, switch statement for each type of operator, run a given function if contains a given operator and run operation
 with parsed numbers
 
+if . in string, parse float
+else parse int
+
+
+add numArray to operators function, conditional that if numArray > 1, evoke the solve function
+
 */
 
-displayBtnActions();
-numberBtnActions();
+btnActions();
+solve();
+
+function solve() {
+
+let equalBtn = document.querySelector(".equal");
+equalBtn.addEventListener("click", () => {
+
+let list = displayScreen.innerText.match(/\d+/g);
+let stringArray = list.sort()
+let numArray = []
+numArray.push(parseInt(stringArray[0]));
+numArray.push(parseInt(stringArray[1]));
+
+if (displayScreen.innerText.match(/\+/) == "+") {
+    displayScreen.innerText = add(numArray[0], numArray[1]);
+} else if (displayScreen.innerText.match(/\-/) == "-") {
+    displayScreen.innerText = subtract(numArray[0], numArray[1]);
+} else if (displayScreen.innerText.match(/\*/) == "*") {
+    displayScreen.innerText = multiply(numArray[0], numArray[1]);
+} else if (displayScreen.innerText.match(/\//) == "/") {
+    displayScreen.innerText = divide(numArray[0], numArray[1]);
+}});
 
 
-
-
-
-
-
-// function solve() {
-
-//     let calcResult =
-// } 
-
-
-let displayScreen = document.querySelector(".current-operation");
-
+}
+function btnActions() {
+    displayBtnActions();
+    numberBtnActions();
+    operatorBtnActions();
+};
 function displayBtnActions() {
 clearDisplay();
 deleteLastChar();
 };
-
 function numberBtnActions() {
 let btns = document.querySelectorAll(".num");
 for (i of btns) {
@@ -52,7 +73,16 @@ for (i of btns) {
     });
   }
 }
-
+function operatorBtnActions() {
+    let btns = document.querySelectorAll(".op");
+    for (i of btns) {
+        i.addEventListener("click", function(e) {
+          console.log(e.target.innerText)
+         
+          displayScreen.innerText += e.target.innerText;
+        });
+      }
+}
 function deleteLastChar () {
 let deleteBtn = document.querySelector(".delete")
 deleteBtn.addEventListener("click", () =>{
@@ -65,32 +95,21 @@ if (displayScreen.innerText.length == 1) {
     }
 })
 }
-
 function clearDisplay() {
 let clearBtn = document.querySelector(".clear")
 clearBtn.addEventListener("click", () => 
     displayScreen.innerText = "0"
 )
 }
-
 function add(x, y) {
 	return x + y
 };
-
 function subtract(x, y) {
 	return x - y
 };
-
 function multiply(x, y) {
   return x * y
 };
-
 function divide(x, y) {
     return x / y
 }
-
-
-// const one = document.querySelectorAll(".btn");
-// console.log(one);
-
-// one.forEach();
