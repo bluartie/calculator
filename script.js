d59a9de3f3520af6305bbb1b350735fa5aa77aed
bluartie <arteaga.jorge3@gmail.com>
2022-11-03 22:54:46 -0500

    remove redundant sort() solves subtraction bug

--- script.js
+++ script.js
@@ -1,48 +1,32 @@
-let displayScreen = document.querySelector(".current-operation");
-
 /*
-create operator functions
-addeventlisteners to buttons, output button events to screen
-
-if more than one operator pressed, run solve()
-
-add eventlisteners to clear screen / delete character
-
-attach solve() to equal sign event listener, display result in current display, and move the equation to the last display
-    attach if statement that if there is no operator, display error
-
-no eval()! instead, function that parses numbers and declare variables for each,
-
-for solve():
-parse operator from display, switch statement for each type of operator, run a given function if contains a given operator and run operation
-with parsed numbers
-
-if . in string, parse float
-else parse int
-
-
-add numArray to operators function, conditional that if numArray > 1, evoke the solve function
-
+when 2 operators or 2 decimals are pressed in a row, display shows NOPE for 1 second
+add an effect to button press
+only one operation at a time allowed
+if NaN, return NOPE
+decimal needs to be allowed
 */
+
+
+let displayScreen = document.querySelector(".current-operation");
 
 btnActions();
 solve();
+
+
 
 function solve() {
 
 let equalBtn = document.querySelector(".equal");
 equalBtn.addEventListener("click", () => {
-
 let list = displayScreen.innerText.match(/\d+/g);
-let stringArray = list.sort()
 let numArray = []
-numArray.push(parseInt(stringArray[0]));
-numArray.push(parseInt(stringArray[1]));
+numArray.push(parseInt(list[0]));
+numArray.push(parseInt(list[1]));
 
 if (displayScreen.innerText.match(/\+/) == "+") {
     displayScreen.innerText = add(numArray[0], numArray[1]);
 } else if (displayScreen.innerText.match(/\-/) == "-") {
-    displayScreen.innerText = subtract(numArray[0], numArray[1]);
+    console.log(displayScreen.innerText = subtract(numArray[0], numArray[1]));
 } else if (displayScreen.innerText.match(/\*/) == "*") {
     displayScreen.innerText = multiply(numArray[0], numArray[1]);
 } else if (displayScreen.innerText.match(/\//) == "/") {
@@ -60,6 +44,7 @@ function displayBtnActions() {
 clearDisplay();
 deleteLastChar();
 };
+
 function numberBtnActions() {
 let btns = document.querySelectorAll(".num");
 for (i of btns) {
