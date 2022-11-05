ac278141d8071b1b11ac242825a50fa7672730ed
bluartie <arteaga.jorge3@gmail.com>
2022-11-05 17:00:27 -0500

    fix dot bug, last thing i need is the negative number bug(using regex conflates negative with subtraction)

--- script.js
+++ script.js
@@ -1,49 +1,28 @@
 /*
-when 2 operators or 2 decimals are pressed in a row, display shows NOPE for 1 second
-add an effect to button press
-only one operation at a time allowed
-if NaN, return NOPE
-decimal needs to be allowed
+allow for decimals and negatives in regex
+    conditional that if a given number (from the array) has a decimal, parseFloat, if not, parseInteger
+
+
 */
 
 
 let displayScreen = document.querySelector(".current-operation");
 
 btnActions();
-solve();
 
-
-
-function solve() {
-
-let equalBtn = document.querySelector(".equal");
-equalBtn.addEventListener("click", () => {
-let list = displayScreen.innerText.match(/\d+/g);
-let numArray = []
-numArray.push(parseInt(list[0]));
-numArray.push(parseInt(list[1]));
-
-if (displayScreen.innerText.match(/\+/) == "+") {
-    displayScreen.innerText = add(numArray[0], numArray[1]);
-} else if (displayScreen.innerText.match(/\-/) == "-") {
-    console.log(displayScreen.innerText = subtract(numArray[0], numArray[1]));
-} else if (displayScreen.innerText.match(/\*/) == "*") {
-    displayScreen.innerText = multiply(numArray[0], numArray[1]);
-} else if (displayScreen.innerText.match(/\//) == "/") {
-    displayScreen.innerText = divide(numArray[0], numArray[1]);
-}});
-
-
-}
 function btnActions() {
     displayBtnActions();
     numberBtnActions();
     operatorBtnActions();
+    equalBtnActions();
+    dotBtnActions();
 };
-function displayBtnActions() {
-clearDisplay();
-deleteLastChar();
-};
+function equalBtnActions() {
+    let equalBtn = document.querySelector(".equal");
+equalBtn.addEventListener("click", () => {
+    solve()
+    document.querySelector(".dot").disabled = false;
+})};
 
 function numberBtnActions() {
 let btns = document.querySelectorAll(".num");
@@ -62,12 +41,37 @@ function operatorBtnActions() {
     let btns = document.querySelectorAll(".op");
     for (i of btns) {
         i.addEventListener("click", function(e) {
-          console.log(e.target.innerText)
-         
+
+         if ((/[\+\-\/\*]/g.test(displayScreen.innerText) == true) && (/[\+\-\/\*]/g.test(displayScreen.innerText.slice(-1)) == false)) {
+            solve();
+            displayScreen.innerText += e.target.innerText;
+         } else if (/[\+\-\/\*\.]/g.test(displayScreen.innerText.slice(-1)) == true) {
+            return;
+         } else {
+            document.querySelector(".dot").disabled = false;
           displayScreen.innerText += e.target.innerText;
-        });
-      }
-}
+        }
+        console.log(/[\+\-\/\*]/g.test(displayScreen.innerText) == true);
+      })
+
+}};
+
+function dotBtnActions () {
+    let dot = document.querySelector(".dot")
+    dot.addEventListener("click", function(e) {
+    if (/[\+\-\/\*\.]/g.test(displayScreen.innerText.slice(-1)) == true) {return}
+    else {
+        document.querySelector(".dot").disabled = true;
+        displayScreen.innerText += e.target.innerText
+    }
+})};
+
+function displayBtnActions() {
+    clearDisplay();
+    deleteLastChar();
+    };
+    
+
 function deleteLastChar () {
 let deleteBtn = document.querySelector(".delete")
 deleteBtn.addEventListener("click", () =>{
@@ -82,10 +86,40 @@ if (displayScreen.innerText.length == 1) {
 }
 function clearDisplay() {
 let clearBtn = document.querySelector(".clear")
-clearBtn.addEventListener("click", () => 
+clearBtn.addEventListener("click", () => {
     displayScreen.innerText = "0"
-)
-}
+    document.querySelector(".dot").disabled = false;
+})}
+
+// /[-]?[0-9]+([\.][0-9]+)?+/g
+///\d+/g
+function solve() {
+    let list = displayScreen.innerText.match(/[-]?[0-9]*\.?[0-9]+/g);
+    console.log(list);
+    let numArray = []
+    if (list[0].includes(".") === true) {
+        numArray.push(parseFloat(list[0]))
+    } else {
+    numArray.push(parseInt(list[0]))
+    }
+    if (list[1].includes(".") == true) {
+        numArray.push(parseFloat(list[0]))
+    } else {
+    numArray.push(parseInt(list[1]))
+    }
+    
+    if (/[\+\-\/\*\.]/g.test(displayScreen.innerText.slice(-1)) == true) {
+        return;} else if (displayScreen.innerText.match(/\+/) == "+") {
+        displayScreen.innerText = add(numArray[0], numArray[1]);
+    } else if (displayScreen.innerText.match(/\-/) == "-") {
+        console.log(displayScreen.innerText = subtract(numArray[0], numArray[1]));
+    } else if (displayScreen.innerText.match(/\*/) == "*") {
+        displayScreen.innerText = multiply(numArray[0], numArray[1]);
+    } else if (displayScreen.innerText.match(/\//) == "/") {
+        displayScreen.innerText = divide(numArray[0], numArray[1]);
+    };
+    }
+
 function add(x, y) {
 	return x + y
 };
